e05b5cc72d207d49f5b1c8f63774a0815216d7e1
OkothPius <orukopius8@gmail.com>
2019-05-21 16:10:21 +0300

    create models

--- instagram/models.py
+++ instagram/models.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
-import datetime as dt 
+from tinymce.models import HTMLField
 
 # Create your models here.
+    
 class Profile(models.Model):
-    profile_photo = models.ImageField('instagram/', null = True)
+    profile_photo = models.ImageField('profile/', null = True)
     bio = models.TextField()
+    user = models.ForeignKey(User)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ['-last_update']
 
     def __str__(self):
-        return self.profile
+        return self.user.username
 
     def save_profile(self):
         self.save()
@@ -18,12 +25,18 @@ class Profile(models.Model):
 
 
 class Image(models.Model):
-    image = models.ImageField('instagram/')
-    image_name = models.CharField(max_length =30)
-    image_caption = models.TextField()
+    image = models.ImageField(upload_to ='image/', null=True)
+    image_name = models.CharField(max_length =30, null=True)
+    image_caption = models.TextField(null=True)
+    user = models.ForeignKey(User, null=True)
     profile = models.ForeignKey(Profile)
     likes = models.IntegerField(default=0)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+
+    class Meta:
+        ordering = ['-pub_date']
 
     def save_image(self):
         self.save()
@@ -31,25 +44,37 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()    
 
-    def __str__(self):
-        return self.image
+    @classmethod
+    def search_by_user(cls,search_term):
+        images = cls.objects.filter(image_caption__icontains=search_term)
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, image_id):
+        images = cls.objects.get(id=image_id)
+        return images   
 
 
 
-class comments(models.Model):
-    comments = models.CharField(null = True, blank=True, max_length= 5000)
-    date = models.TextField(blank=True, null=True)
+class Comment(models.Model):
+    comment = models.CharField(null = True, max_length= 5000, verbose_name = 'name')
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, null=True)
     image = models.ForeignKey(Image, null= True)
 
-    def save_comments(self):
+    class Meta:
+        verbose_name = "comments"
+        verbose_name_plural = "comments"
+
+    class Meta:
+        ordering = ['-date']        
+
+    def save_comment(self):
         self.save()
 
-    def delete_comments(self):
+    def delete_comment(self):
         self.delete()
             
 
 
-    # @classmethod
-    # def search_by_image_category(cls,search_term):
-    #     pictures = cls.objects.filter(image_name__icontains=search_term)
-    #     return picturescomments
+    

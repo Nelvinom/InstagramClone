5752c6862803f9c69327a2bf804be16b03b64bc2
OkothPius <orukopius8@gmail.com>
2019-05-21 16:08:28 +0300

    register models

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Image, comments
+from .models import Profile, Image
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
-admin.site.register(comments)
 
 

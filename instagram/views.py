ceb3f6a6bfd01df38b7e37bce7895b1a9a94ba57
OkothPius <orukopius8@gmail.com>
2019-05-21 16:18:19 +0300

    configure routes

--- instagram/views.py
+++ instagram/views.py
@@ -1,24 +1,81 @@
+from django.contrib.auth.decorators import login_required
+from .forms import  NewStatusForm, NewCommentForm
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
-from .models import Image
+from .models import Image, Profile, Comment
 
 # Create your views here.
-def instagram(request):
-    return render(request, 'instagram.html', {"image":image,})
+@login_required(login_url='/accounts/login/')
+def timeline(request):
+    current_user = request.user
+    images = Image.objects.order_by('-pub_date')
+    profiles = Profile.objects.order_by('-last_update')
+    comment = Comment.objects.order_by('-date')
+    return render(request, 'timeline.html', {"images":images, "profiles":profiles, "user_profile":user_profile, "comment":comment})
 
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    # profile = Profile.objects.get(user_id=current_user.id)
+    images = Image.objects.all().filter(profile_id=current_user.id)
+    return render(request, 'profile.html', {"images":images, "profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def new_status(request, username):
+    current_user = request.user
+    username = current_user.username
+    if request.method =='POST':
+        form = NewStatusForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save()
+            image.user = request.user
+            image.save()
+        return redirect('allTimelines')
+    else:
+        form = NewStatusForm()
+    return render(request, 'new_status.html', {"form":form}) 
+
+#User Profile           
+@login_required(login_url='/accounts/login/')
+def user_profile(request, user_id):
+    profile = Profile.objects.get(id=user_id)
+    images = Image.objects.all().filter(user_id=user_id)
+    return render(request, "profile.html", {"[profile":profile, "images":images})
+
+@login_required(login_url='/accounts/login/')
 def single_image(request, image_id):
     image = Image.objects.get(id = image_id)
-    return render(request, "image.html",{"image":image})    
+    return render(request, "single_image.html",{"image":image})
 
-
-def search_results(request):
+def find_profile(request):
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_by_image_category(search_term)
+        searched_images = Image.search_by_user(search_term)
         message = f"{search_term}"
-
-        return render(request, 'search.html',{"message":message,"image": searched_images})
+        return render(request, 'user_profile.html',{"message":message,"image": searched_images})
 
     else:
         message = "You haven't searched for any term yet"
-        return render(request, 'search.html',{"message":message})    
+        return render(request, 'single_image.html',{"message":message}) 
+
+@login_required(login_url='/accounts/login/')
+def single_image_like(request, image_id):
+    image = Image.objects.get(id=image_id)
+    image.likes = image.likes + 1
+    image.save()
+    return redirect('allTimelines')
+
+@login_required(login_url='/accounts/login/')
+def new_comment(request, username):
+    current_user =request.user
+    username = current_user.username
+    if request.method =='POST':
+        form = NewCommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save()
+            comment.user = request.user
+            comment.save()
+        return redirect('allTimelines')
+    else:
+        form = NewCommentForm()
+    return render(request, 'new_comment.html', {"form":form})
+
